973f4c5fde166efab217e0124fb6f13950b49101
Pedro Lima <pedro.oficial1969@gmail.com>
2022-04-13 18:24:12 -0300

    refactor: adiciona try/catch no middleware do login.

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -4,24 +4,24 @@ const loginServices = require('../services/login.services');
 const { JWT_SECRET } = process.env;
 
 const loginController = async (req, res) => {
-  const { email } = req.body;
-  const data = await loginServices.loginServices(email);
-  if (!data) return res.status(400).json({ message: 'Invalid fields' });
-
-  const payload = {
-    displayName: req.body.displayName,
-  };
-
-  const token = jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '1h',
-    algorithm: 'HS256',
-  });
-
-  return res.status(200).json({ token });
-/*   try {
+  try {
+    const { email } = req.body;
+    const data = await loginServices.loginServices(email);
+    if (!data) return res.status(400).json({ message: 'Invalid fields' });
+  
+    const payload = {
+      displayName: req.body.displayName,
+    };
+  
+    const token = jwt.sign(payload, JWT_SECRET, {
+      expiresIn: '1h',
+      algorithm: 'HS256',
+    });
+  
+    return res.status(200).json({ token });
   } catch (error) {
     return res.status(500).json({ message: 'Internal Server Error' });
-  } */
+  }
 };
 
 module.exports = {
