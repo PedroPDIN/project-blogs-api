e3b3950d4e174494a6da0248e171b3d677818979
Pedro Lima <pedro.oficial1969@gmail.com>
2022-10-07 10:10:29 -0300

    fix: alteração minimalista no index.

--- index.js
+++ index.js
@@ -8,18 +8,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/user', auth, user.getAllUsersController);
-app.get('/categories',
-auth,
-category.getAllCategoryController);
 app.get('/user/:id', auth, user.getIdUserController);
+app.get('/categories',auth, category.getAllCategoryController);
 app.get('/post', auth, post.postAllController);
 app.get('/post/:id', auth, post.postIdController);
 
 app.post('/user', validate.isValidUser, user.createUserController);
 app.post('/login', validate.isValidLogin, login.loginController);
-app.post('/categories',
-auth,
-validate.isValidCategory, 
-category.createCategoryController);
+app.post('/categories', auth, validate.isValidCategory, category.createCategoryController);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
